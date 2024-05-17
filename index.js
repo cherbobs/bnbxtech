77c894906054d64606e083c7db932bb62a1117e0
cherbobs <eva.cherbonnier974@gmail.com>
2024-05-17 10:32:53 +0200

    animation typewriter ok

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
+
+            
+            let valueDisplays = document.querySelectorAll(".num"); // Change to querySelectorAll to select all elements with class "num"
+            let interval = 3500;
+
+            valueDisplays.forEach((valueDisplay) => {
+                let startValue = 0;
+                let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+                let duration = Math.floor(interval / endValue);
+                let counter = setInterval(function() {
+                    startValue += 1;
+                    valueDisplay.textContent = startValue;
+                    if (startValue == endValue) {
+                        clearInterval(counter);
+                    }
+                }, duration);
+            });
+
         } else {
             entry.target.classList.remove('show');
         }
@@ -11,18 +29,13 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
-let valueDisplays = document.querySelectorAll(".num"); // Change to querySelectorAll to select all elements with class "num"
-let interval = 3500;
 
-valueDisplays.forEach((valueDisplay) => {
-    let startValue = 0;
-    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-    let duration = Math.floor(interval / endValue);
-    let counter = setInterval(function() {
-        startValue += 1;
-        valueDisplay.textContent = startValue;
-        if (startValue == endValue) {
-            clearInterval(counter);
-        }
-    }, duration);
-});
+
+var typed = new Typed(".text-wrapper-6",{
+    strings: ["Grands groupes", "Start-up", "Secteurs publiques", "PME", "Ministères"],
+    typeSpeed : 150,
+    backSpeed : 150,
+    loop: true,  
+    showCursor: false, 
+  
+})
